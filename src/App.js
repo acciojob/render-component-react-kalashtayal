faf04e97b6e1fc29dc36cb1bae5bdaf3835e7ff3
tayal <kalashtayal2000@gmail.com>
2023-07-13 18:22:51 +0530

    submitting code-Thu Jul 13 2023 18:22:49 GMT+0530 (India Standard Time)

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = ()=> {
+     
+    return(
+        <div>
+      <p>"I am learning React. My life is getting better."</p>
+        </div>
+    )
+}
+export default App;
