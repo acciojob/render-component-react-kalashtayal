265add7d1a859ea3f00a4bc6c8164bb3f926ec0e
tayal <kalashtayal2000@gmail.com>
2023-07-13 19:30:42 +0530

    submitting code-Thu Jul 13 2023 19:30:41 GMT+0530 (India Standard Time)

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const App = () => {
       <div>
         <p>I am learning React. My life is getting better.</p>
       </div>
-    );
+    )
   };
 
 ReactDOM.render( <App /> , document.getElementById("root"));
