510997231efe73f7386a2653cffab0fa0aabf2f1
tayal <kalashtayal2000@gmail.com>
2023-07-13 18:34:06 +0530

    submitting code-Thu Jul 13 2023 18:34:04 GMT+0530 (India Standard Time)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+
+const App = () => {
+    return (
+      <div>
+        <p>I am learning React. My life is getting better.</p>
+      </div>
+    );
+  };
 
 ReactDOM.render( < App/> , document.getElementById("root"));
