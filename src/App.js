dc9523eae39a095a144929eb4b17f826cacaad0f
tayal <kalashtayal2000@gmail.com>
2023-07-16 11:37:08 +0530

    submitting code-Sun Jul 16 2023 11:37:06 GMT+0530 (India Standard Time)

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const App=()=>{
+    return <div>
+        <p>I am learning React. My life is getting better.</p>
+      
+    </div>
+}
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
 
-const App = () => {
-    return (
-      <div>
-        <p>I am learning React. My life is getting better.</p>
-      </div>
-    )
-  };
+
 
 ReactDOM.render( <App /> , document.getElementById("root"));
 
-export default App;
